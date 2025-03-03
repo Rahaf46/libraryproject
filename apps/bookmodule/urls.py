076d3b260ee7075115deaf18ac633bea180e4f01
Rahaf46 <masalhh11@gmail.com>
2025-03-03 22:01:55 +0300

    lab4

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),   
-    path('index2/<int:val1>/', views.index2),  
-    path('<int:bookId>/', views.viewbook)
+    path('', views.index, name='books.index'),
+    path('list/', views.list_books, name='books.list_books'),
+    path('book/<int:bookId>/', views.viewbook1, name='books.view_one_book'),
+    path('about/', views.about_us, name='books.aboutus'),
 ]
-
-
-
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -2,31 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    name = request.GET.get("name") or "world"
-    return render(request, "bookmodule/index.html" , {"name": name})
+    return render(request, "bookmodule/index.html")
 
-def index2(request, val1=0): 
-    
-    try:
-        val1 = int(val1)  
-    except ValueError:
-        return HttpResponse("error, expected val1 to be integer")  
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
 
-    return render(request, "bookmodule/index2.html", {"val1": val1})  
+def viewbook1(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
 
-def viewbook(request, bookId):
-
-    book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
-    book2 = {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'}
-    
-    targetBook = None
-    if book1['id'] == bookId:
-        targetBook = book1
-    if book2['id'] == bookId:
-        targetBook = book2
-    
-    if targetBook is None:
-        return HttpResponse("Book not found")
-    
-    context = {'book': targetBook}
-    return render(request, 'bookmodule/show.html', context)
+def about_us(request):
+    return render(request, 'bookmodule/aboutus.html')

7cab17d44cab95407c2a3231e4cb981c5f5611b0
Rahaf46 <masalhh11@gmail.com>
2025-03-10 15:31:35 +0300

    lab 5 & lab 6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('list/', views.list_books, name='books.list_books'),
     path('book/<int:bookId>/', views.viewbook1, name='books.view_one_book'),
     path('about/', views.about_us, name='books.aboutus'),
+    path('html5/links/', views.html5_links, name='html5_links'), 
+    path('html5/text/formatting/', views.text_formatting, name='text_formatting'),
+    path('html5/listing/', views.listing_page, name='html5_listing'),
+    path('html5/tables/', views.tables_page, name='html5_tables'),
+    path("search/", views.search_books, name="search_books"),
+    
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -12,3 +12,45 @@ def viewbook1(request, bookId):
 
 def about_us(request):
     return render(request, 'bookmodule/aboutus.html')
+
+def html5_links(request):
+    return render(request, 'bookmodule/html5_links.html') 
+
+def text_formatting(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def listing_page(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables_page(request):
+    return render(request, 'bookmodule/tables.html')
+
+
+def search_books(request):
+    if request.method == "POST":
+        keyword = request.POST.get('keyword', '').lower()
+        is_title = request.POST.get('option1') 
+        is_author = request.POST.get('option2') 
+        
+        books = __getBooksList()
+        filtered_books = []
+
+        for book in books:
+            found = False
+            if is_title and keyword in book['title'].lower():
+                found = True
+            if not found and is_author and keyword in book['author'].lower():
+                found = True
+
+            if found:
+                filtered_books.append(book)
+
+        return render(request, 'bookmodule/bookList.html', {'books': filtered_books})
+
+    return render(request, "bookmodule/search.html")
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
